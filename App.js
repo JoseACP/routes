fb151434f794286c21a5fc04bf9fafbbaf783b3d
JoseACP <jose.chairez.is@unipolidgo.edu.mx>
2024-04-27 18:33:21 -0600

    main

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity,Image } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView 
+    style={{
+      flex:1, 
+      justifyContent: 'center', 
+      alignItems:'center',
+      backgroundColor:'#ffffff'}}
+      > 
+      <View>
+      
+        <Text style={{fontSize:30, fontWeight:'bold', color:'#20315f'}}>
+          GAMEON
+        </Text>
+      
+      </View>
+      <Image
+        source={require('./assets/images/dualsense.png')}
+        style={{ width: 370,
+          height: 250,
+          resizeMode: 'cover'}}
+      />
+      <TouchableOpacity style={{backgroundColor:'#AD40AF', padding:20, width:'90%', borderRadius:5, flexDirection:'row', justifyContent:'space-between'}}>
+        <Text style={{fontWeight:'bold', fontSize:18, color:'#fff'}}>let´s begin</Text>
+        <MaterialIcons name="arrow-forward-ios" size={22} color='#fff'/>
+      </TouchableOpacity>
+    </SafeAreaView>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+;
